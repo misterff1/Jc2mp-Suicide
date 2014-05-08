b16173b3fb466887c87a43d0fb478d108004e496
misterff1 <misterff1@gmail.com>
2014-05-08 10:53:32 +0200

    Added commentary

--- client/Suicide.lua
+++ client/Suicide.lua
@@ -2,7 +2,6 @@
 
 
 function SuicideStart(pos)
-   ClientEffect.Play(AssetLocation.Game, { effect_id = 75, position = pos, angle = Angle() })
-end
+   ClientEffect.Play(AssetLocation.Game, { effect_id = 75, position = pos, angle = Angle() }) -- Change the effect_id to something else to change the effect
 
 Network:Subscribe("SuicideStart", SuicideStart)
